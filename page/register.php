c18fd6c6777baf28d9d3bf154413303b46c807e6
bover <59316027+boversoneg@users.noreply.github.com>
2021-06-18 10:09:15 +0200

    text center in register page

--- page/register.php
+++ page/register.php
@@ -82,7 +82,7 @@
         <title>Panel rejestracji</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <style>
-            body{ font: 14px sans-serif; }
+            body{ font: 14px sans-serif; text-align: center; }
             .wrapper{ width: 360px; padding: 20px; }
             .dark-mode {
                     background-color: #1a1919;
@@ -91,30 +91,28 @@
         </style>
     </head>
     <body>
-        <div class="wrapper">
-            <h2>Panel rejestracji</h2>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <div class="form-group">
-                    <label>Nazwa użytkownika</label>
-                    <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                    <span class="invalid-feedback"><?php echo $username_err; ?></span>
-                </div>    
-                <div class="form-group">
-                    <label>Hasło</label>
-                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
-                </div>
-                <div class="form-group">
-                    <label>Potwierdź hasło</label>
-                    <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                    <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-                </div>
-                <div class="form-group">
-                    <input type="submit" class="btn btn-primary" value="Zarejestruj">
-                </div>
-                <p>Masz konto? <a href="login.php">zaloguj się</a>.</p>
-            </form>
-        </div>    
+        <h2>Panel rejestracji</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group" >
+                <label>Nazwa użytkownika</label><br>
+                <input type="text" style='width: 360px; vertical-align: middle; display:table-cell;' name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Hasło</label><br>
+                <input type="password" style='width: 360px; vertical-align: middle; display:table-cell;' name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Potwierdź hasło</label><br>
+                <input type="password" style='width: 360px; vertical-align: middle; display:table-cell;' name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Zarejestruj">
+            </div>
+            <p>Masz konto? <a href="login.php">zaloguj się</a>.</p>
+        </form>   
     </body>
     <script>
         (function() {
